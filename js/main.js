0c0cbf78b21a0904e3a7650b4f6f2d421aaadec1
nagsaw <nagsaw1989@gmail.com>
2022-01-01 16:59:43 +0900

    名前を変数nameに入れて自己紹介をテンプレートリテラルに変更 (+2 squashed commits) Squashed commits: [90fb449] initに自己紹介を追加 [826bf47] init関数を再度追加

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 'use strict';
+const name = 'nagsaw';
+function init() {
+  console.log('initialized...');
+  console.log(`my name is ${name}`);
+}
+document.addEventListener('DOMContentLoaded', init);
