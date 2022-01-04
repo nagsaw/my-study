135e99e356fcd13cef0d48bc1f4b382eb832336e
nagsaw <nagsaw1989@gmail.com>
2022-01-04 15:30:31 +0900

    index.htmlにh1を追加 main.jsでh1変数を追加

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 'use strict';
-const name = 'nagsaw';
+let h1;
 function init() {
   console.log('initialized...');
-  console.log(`my name is ${name}`);
+  h1 = document.querySelector('h1');
+  console.log(h1);
 }
 document.addEventListener('DOMContentLoaded', init);
